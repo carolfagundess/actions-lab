2712a32c5136d25c45aab2338c76c0625bb94973
Carol Fagundes <carolinarftk@gmail.com>
2026-09-10 22:41:19 -0300

    test: env e secrets

--- soma.js
+++ soma.js
@@ -10,3 +10,7 @@ if (soma(2, 3) !== 5) {
 }
 
 console.log('Tudo certo!');
+
+// Novo: lendo variáveis de ambiente
+console.log('Ambiente (env comum):', process.env.AMBIENTE_LAB);
+console.log('Segredo existe?', process.env.SEGREDO_LAB ? 'sim' : 'não');
