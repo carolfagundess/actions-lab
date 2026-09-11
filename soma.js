7c40521009a5eb1fe1d119f8af39b7cd4adb28b4
Carol Fagundes <carolinarftk@gmail.com>
2026-09-10 22:37:28 -0300

    test: rodar Node de verdade no Actions

---
+++ soma.js
@@ -0,0 +1,12 @@
+function soma(a, b) {
+  return a + b;
+}
+
+console.log('2 + 3 =', soma(2, 3));
+
+if (soma(2, 3) !== 5) {
+  console.error('Erro: a soma deu errado!');
+  process.exit(1); // isso é o que faz o job do Actions falhar de propósito, se quiser testar
+}
+
+console.log('Tudo certo!');
